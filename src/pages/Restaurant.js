4dc36c8982fca5b36f26c8ebe5b28f4231ab09c1
rosangelabsilva <rosangela-bsilva@hotmail.com>
2020-01-06 23:42:19 -0300

    fuction addOrder and total price

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -6,6 +6,7 @@ import Menu from "../componentes/menu";
 
 function Restaurant () {
     const [data, setData] = useState([]);
+    const [order, setOrder] = useState([]);
     useEffect(() => {
       firebase.firestore().collection('Menu').get()
       .then((snapshot) => {
@@ -14,26 +15,16 @@ function Restaurant () {
           ...doc.data()
         }))
         setData(itensMenu) 
-      })
+      }) 
     }, [])
 
-    function addOrder(item){
-      const itemIndex = data.findIndex((el) => el.id === item.id);
-      
-      if(itemIndex === -1){
-        setData([...data, { ...item, count: 1 }])
-      } else {
-        const newOrder = [...data];
-        newOrder[itemIndex].count += 1;
-        setData(newOrder)
-        console.log(newOrder)
-      }
-      // setTotal(total + (item.Price ))     
+    const addOrder = (item) => {
+
+      setOrder([...order, item])     
     }
 
-    const menuCoffee = data.filter(item => !item.Coffee)
-    const menuRest = data.filter(item => item.Coffee)
-
+    const total = order.reduce((accumulator, item) => accumulator + item.Price, 0)
+    
     return(
     <>
         <h1>Salão</h1>
@@ -45,16 +36,33 @@ function Restaurant () {
     </form>
     <h2>Café da manhã</h2>
     <div className= {css(styles.div)}>
-      {menuCoffee.map((item) =><Menu item={item} addOrder={addOrder}/>)}
+      {data.map((item) => {  
+      if (item.Coffee){
+        return <Menu item={item} addOrder={addOrder}/>
+      } return ""
+    })}
     </div>
   <h2>Resto do dia</h2>
   <div className= {css(styles.div)}>
-    {menuRest.map((item) =><Menu item={item} addOrder={addOrder}/>)} 
+    {data.map((item) => {
+      if (!item.Coffee){
+        return <Menu item={item} addOrder={addOrder}/>
+      } return ""
+    } 
+  )}
   </div>
+
+  {/* inicio pedido */}
   <h3>Pedidos</h3>
   <div>
-    {data.map(item =>
-      <span> {item.Name} {item.Price} </span>)}
+    {order.map(item =>
+      <span> {item.Name}
+            {item.Price.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})} 
+        {item.Options.toLocaleString()}
+      <br/></span>
+    )}
+    <br/>
+    <span>Total: R$ {total}</span>
   </div>
   </>
     )
