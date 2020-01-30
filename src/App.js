5dfb1204aa7d0551bd2774876c3e96401dbf97ca
rosangelabsilva <rosangela-bsilva@hotmail.com>
2020-01-30 14:32:14 -0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import Logo from './componentes/logo';
-import Naveg from './componentes/Naveg';
+import Restaurant from "./pages/Restaurant";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import Kitchen from './pages/Kitchen';
+import { StyleSheet, css } from 'aphrodite';
 
 function App() {
 
   return ( 
     <>
-    <div>
       <section>
         <Logo/>
       </section>
-      <Naveg/>
-    </div>
+      <Router>
+          <nav>
+            <ul className= {css(styles.ul)}>
+              <li>
+                <Link to="/">Salão</Link>
+              </li>
+              <li>
+                <Link to="/kitchen">Cozinha</Link> 
+              </li>
+            </ul>
+          </nav>
+          
+          <Route exact path="/" component={Restaurant}/>
+          <Route exact path="/kitchen" component={Kitchen}/>
+        </Router>
   </>
   )
 }
+
+const styles = StyleSheet.create({
+  ul: {
+    position: 'absolute',
+    left: '20%',
+    fontSize: 35,
+    textAlign: 'center',
+    color: '#DCDCDC',
+    display: 'flex',
+    margin: 60,
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    height: '10%',
+    width: '50%',
+    top: 1,
+  },
+})
 
 export default App
